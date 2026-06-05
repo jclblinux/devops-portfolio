93d2a70f85f9229587139ebf9bf7b61ceb842bfd
Jakub Horak <jclblinux@seznam.cz>
2026-06-05 10:47:59 +0000

    Add nodeapp Helm chart, Ansible role, and Security Group port 30090

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "sec_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Nodeapp"
+    from_port   = 30090
+    to_port     = 30090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
